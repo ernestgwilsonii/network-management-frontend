a207cea5826b4f27bef3f9443c15998b6d2f2292
Ernie Wilson <ernestgwilsoniii@gmail.com>
2016-08-19 23:43:14 -0400

    Minor changes

--- src/controllers/home/homeController.js
+++ src/controllers/home/homeController.js
@@ -2,10 +2,6 @@ export default class HomeController {
     constructor() {
         
     }
-
-    welcome() {
-        return "Welcome to the Network Management App!"
-    }
 }
 
 HomeController.$inject = [];
